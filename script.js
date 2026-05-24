18a9a22419435033c7975e7aef738e9b2f171c12
Luan Nogueira <luan.cavalcante@eaportal.org>
2026-05-23 21:04:05 -0300

    sétimo commit do HTML e primeiro commit do JS

---
+++ script.js
@@ -0,0 +1,26 @@
+let conta = 0
+let pesssoas = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
+
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", )
+
+function receberQuantidadePessoas(evento){
+     const paragrafoErro = document.querySelector(".pessoas #erro")
+     const divErro = document.querySelector(".pessoas .input-box")
+
+    if(evento.target.value === "0") {    
+        paragrafoErro.styles.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    } else {
+        paragrafoErro.styles.display = "block"
+        divErro.setAttribute("id", "")
+        pesssoas = Number(evento.target.value)
+    }
+}
