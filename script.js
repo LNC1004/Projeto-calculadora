a8fe577f7a63fea0ec3dd7cc9333e6fc588f8dee
Luan Nogueira <luan.cavalcante@eaportal.org>
2026-05-23 21:37:26 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -31,7 +31,14 @@ botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", )
 })
 
-function receberPorcentagem() {
-    console.log(evento.target.value)
+function receberPorcentagemBotao() {
+botoesGorjeta.forEach(botao => {
+botao.classList.remove("botao-ativo")
 
+    if(botao.value === evento.target.value) {
+        botao.classList.add("botao-ativo")
+    }
+})
+
+    porcentagem = parseFloat(evento.target.value) / 100
 }
